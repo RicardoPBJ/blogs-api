d35e5eef0def2979100fb6e05d33609393586bcb
Ricardo <rbrumjunio@gmail.com>
2023-04-28 14:00:14 -0300

    Crie o modelo PostCategory em src/models/PostCategory.js com as propriedades e associações corretas

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+	const PostCategoryTable = sequelize.define('PostCategory', {
+		postId: {
+			type: DataTypes.INTEGER,
+      field: 'user_id',
+			allowNull: false,
+		},
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'category_id',
+    },
+  }, {
+		tableName: 'posts_categories',
+		underscored: true,
+		timestamps: false,
+	});
+
+  PostCategoryTable.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategoryTable,
+      foreignKey: 'postId',
+      otherkey: 'categoryId',
+      as: 'categories',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategoryTable,
+      foreignKey: 'categoryId',
+      otherkey: 'postId',
+      as: 'posts',
+    });
+  }
+
+	return PostCategoryTable;
+};
